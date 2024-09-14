d22267c48f3dd88085029e89e5cfbbd0e8ebafc6
Ivan Lavrenov <visperas1703@gmail.com>
2024-09-14 18:16:24 +0500

    feat: index html (Sat, Sep 14, 2024 18:15:57 PM)

--- cssMemeSlider/index.js
+++
@@ -1,8 +0,0 @@
-function getFormattedDate() {
-    const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
-    const date = new Date();
-    return date.toLocaleString('en-EN', options);
-}
-
-const DATE_STRING = getFormattedDate();
-console.log(DATE_STRING);
