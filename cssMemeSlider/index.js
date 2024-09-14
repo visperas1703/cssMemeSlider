d61a96b6d06e94264f3eb73aa52d0814890eeeb7
Ivan Lavrenov <visperas1703@gmail.com>
2024-09-14 20:58:09 +0500

    feat: js components (Sat, Sep 14, 2024 20:56:02 PM)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,43 @@
+const sliderLine = document.querySelector('.slider-line');
+const controls = document.querySelectorAll('.control');
+const slideCaption = document.getElementById('slide-caption');
+
+const captions = [
+    'Великий комбинатор!',
+    'Аристократ!',
+    'Налёт на психушку!!',
+    'Ночная тварь...',
+    'Серцеед'
+];
+
+let currentIndex = 0;
+
+controls.forEach((control, index) => {
+    control.addEventListener('click', () => {
+        moveSliderTo(index);
+    });
+});
+
+function moveSliderTo(index) {
+    currentIndex = index;
+    
+    const sliderWidth = document.querySelector('.slider').offsetWidth;
+    const offset = -sliderWidth * index;
+    
+    sliderLine.style.transform = `translateX(${offset}px)`;
+
+    updateActiveControl(index);
+    updateSlideCaption(index); 
+}
+
+function updateActiveControl(index) {
+    controls.forEach(control => control.classList.remove('active'));
+    controls[index].classList.add('active');
+}
+
+function updateSlideCaption(index) {
+    slideCaption.textContent = captions[index];
+}
+
+updateActiveControl(0);
+updateSlideCaption(0);
